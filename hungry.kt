4a7b00ee2c7c8fcb4b054113dc96f7309f7c84a4
Yash Jain <yash.jain6350@gmail.com>
2018-11-11 22:47:26 +0530

    conditios added in hungry code

--- hungry.kt
+++ hungry.kt
@@ -1,7 +1,21 @@
 fun main(args : Array<String>) {
-    print()
+
+    println("Write yes or no if you are hungry")
+
+    val input = readLine()!!
+    print(input)
 }
 
-fun print() {
-    print("I am hungry")
+fun print(input: String) {
+
+    if(input == "yes") {
+        print("I am hungry. I will eat samosa")
+    }
+    else if(input == "no") {
+        print("I am not hungry. I will do my work")
+    }
+    else {
+        print("Invalid option")
+    }
+
 }
