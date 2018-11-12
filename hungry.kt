59f73bab50417ae5077a01b5ece56a6be5846e8f
Yash Jain <yash.jain6350@gmail.com>
2018-11-12 23:47:20 +0530

    sev-puri

--- hungry.kt
+++ hungry.kt
@@ -10,6 +10,7 @@ fun print(input: String) {
 
     if(input == "yes") {
         print("I am hungry. I will eat samosa")
+        print("I am hungry. I will eat sev-puri")
     }
     else if(input == "no") {
         print("I am not hungry. I will do my work")
