4a0fff43a41981ae0ad2be1b50564c58faa30e2c
Yash Jain <yash.jain6350@gmail.com>
2018-11-13 22:59:43 +0530

    veg-kolhapuri-new

--- hungry.kt
+++ hungry.kt
@@ -10,8 +10,11 @@ fun print(input: String) {
 
     if(input == "yes") {
         print("I am hungry. I will eat samosa")
+<<<<<<< HEAD
         print("I am hungry. I will eat sev-puri")
         print("I am hungry. I will eat veg-kolhapuri")
+=======
+>>>>>>> parent of 59f73ba... sev-puri
     }
     else if(input == "no") {
         print("I am not hungry. I will do my work")
